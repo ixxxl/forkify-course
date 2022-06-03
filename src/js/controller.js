e044148530139199082da1c8e4dc5c805db71a39
ixxxl <ixxxl@mail.ru>
2022-06-03 20:20:43 +0300

    added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -122,6 +122,9 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log(`Welcome to the application`);
+};
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -130,6 +133,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log(`Welcome`);
+  newFeature();
 };
 init();
